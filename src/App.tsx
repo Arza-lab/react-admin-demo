5b5fea3da2220c31baf42e3883c803050f195b30
Maurice Schwarz <schwarz@desginpark.de>
2022-05-26 15:30:53 +0200

    feat: added dummy auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import {lightTheme} from './layout/theme';
+import authProvider from "./auth/authProvider";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const App = () => (
-      <Admin dataProvider={dataProvider} theme={lightTheme}>
+      <Admin dataProvider={dataProvider} authProvider={authProvider} theme={lightTheme}>
           <Resource name="users" list={ListGuesser} edit={EditGuesser} />
           <Resource name="posts" list={ListGuesser} edit={EditGuesser} />
           <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
