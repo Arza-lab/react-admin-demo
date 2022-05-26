8dda5c930b1cfc5c8dc5839e34b14ff93ecf4bd7
Maurice Schwarz <schwarz@desginpark.de>
2022-05-26 14:57:36 +0200

    init + react-admin packages

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+
+const App = () => (
+      <Admin dataProvider={dataProvider}>
+          <Resource name="users" list={ListGuesser} edit={EditGuesser} />
+          <Resource name="posts" list={ListGuesser} edit={EditGuesser} />
+          <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+          <Resource name="photos" list={ListGuesser} edit={EditGuesser} />
+      </Admin>
+  );
+
+export default App;
